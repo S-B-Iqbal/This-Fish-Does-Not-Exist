963f91281dfdd934cb609d7284eb77fad70a3f10
S-B-Iqbal <buland.shahrukh@gmail.com>
2021-09-16 10:55:33 +0200

    Create plotting.py

---
+++ thisfishdoesnotexist/plotting.py
@@ -0,0 +1,56 @@
+    """Plotting Helper Functions"""
+import numpy as np
+import matplotlib.pyplot as plt
+def plot_multiple_training_losses(losses_list, num_epochs, 
+                                  averaging_iterations=100, custom_labels_list=None):
+
+    for i,_ in enumerate(losses_list):
+        if not len(losses_list[i]) == len(losses_list[0]):
+            raise ValueError('All loss tensors need to have the same number of elements.')
+    
+    if custom_labels_list is None:
+        custom_labels_list = [str(i) for i,_ in enumerate(custom_labels_list)]
+    
+    iter_per_epoch = len(losses_list[0]) // num_epochs
+
+    plt.figure()
+    ax1 = plt.subplot(1, 1, 1)
+    
+    for i, minibatch_loss_tensor in enumerate(losses_list):
+        ax1.plot(range(len(minibatch_loss_tensor)),
+                 (minibatch_loss_tensor),
+                  label=f'Minibatch Loss{custom_labels_list[i]}')
+        ax1.set_xlabel('Iterations')
+        ax1.set_ylabel('Loss')
+
+        ax1.plot(np.convolve(minibatch_loss_tensor,
+                             np.ones(averaging_iterations,)/averaging_iterations,
+                             mode='valid'),
+                 color='black')
+    
+    if len(losses_list[0]) < 1000:
+        num_losses = len(losses_list[0]) // 2
+    else:
+        num_losses = 1000
+    maxes = [np.max(losses_list[i][num_losses:]) for i,_ in enumerate(losses_list)]
+    ax1.set_ylim([0, np.max(maxes)*1.5])
+    ax1.legend()
+
+    ###################
+    # Set scond x-axis
+    ax2 = ax1.twiny()
+    newlabel = list(range(num_epochs+1))
+
+    newpos = [e*iter_per_epoch for e in newlabel]
+
+    ax2.set_xticks(newpos[::10])
+    ax2.set_xticklabels(newlabel[::10])
+
+    ax2.xaxis.set_ticks_position('bottom')
+    ax2.xaxis.set_label_position('bottom')
+    ax2.spines['bottom'].set_position(('outward', 45))
+    ax2.set_xlabel('Epochs')
+    ax2.set_xlim(ax1.get_xlim())
+    ###################
+
+    plt.tight_layout()
